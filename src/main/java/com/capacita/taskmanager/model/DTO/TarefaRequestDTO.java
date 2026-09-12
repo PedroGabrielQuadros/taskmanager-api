f33e8c26900da76450d6678e87fffe496996077a
Pedro Quadros <gabrielx2204@gmail.com>
2026-09-12 17:05:38 -0300

    criacao dos DTOs da Tarefa

---
+++ src/main/java/com/capacita/taskmanager/model/DTO/TarefaRequestDTO.java
@@ -0,0 +1,20 @@
+package com.capacita.taskmanager.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class TarefaRequestDTO {
+
+    @NotBlank(message = "O título é obrigatório")
+    @Size(max = 100, message = "O título não pode ultrapassar 100 caracteres")
+    private String titulo;
+
+    private String descricao;
+
+    public TarefaRequestDTO() {}
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+}
